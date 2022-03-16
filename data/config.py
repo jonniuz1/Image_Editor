418e70e96760a162f8a00975997000a2b2d0922e
jonniuz1 <joevega3574@gmail.com>
2022-03-17 00:14:54 +0500

    Initial commit

--- data/config.py
+++ data/config.py
@@ -14,6 +14,6 @@
 import os
 
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-ADMINS = str(os.environ.get("ADMINS"))
+ADMINS = int(os.environ.get("ADMINS"))
 IP = str(os.environ.get("ip"))
 API_TOKEN = str(os.environ.get("API_TOKEN"))
